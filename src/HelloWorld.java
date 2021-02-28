e88df03156e03eeca4fc1d92449ecf3f9131eba6
sergey <barmaley0o@gmail.com>
2021-02-28 23:19:04 +0300

    Задача 3. Массивы

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,13 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        double a = 123.37484;
+        byte b = 127;
+
+        int[] array = new int[]{14, 8, 3, 195, 52};
+        int c = array[array.length-1];
+        array [array.length-1] = array[0];
+        array[0] = c;
+        System.out.println(array[0] + (array.length-2));
     }
-    double A = 123.37484;
-    byte B = 127;
 }
